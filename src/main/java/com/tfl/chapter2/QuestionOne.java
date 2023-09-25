c8406d4159baf32ac3bc126df659fa5119fd2a94
xiaoliang <651147191@qq.com>
2023-09-25 16:59:06 +0800

    数组中三数和为零的不重复数组合

--- src/main/java/com/tfl/chapter2/QuestionOne.java
+++ src/main/java/com/tfl/chapter2/QuestionOne.java
@@ -1,4 +1,27 @@
 package com.tfl.chapter2;
 
+/**
+ * 问题：给出排好序的整数数组和一个指定的数k，求出数组中和为k的两数的下标，整数不重复且只有一对整数的和为k。
+ * 解题思路：使用双指针实现
+ */
 public class QuestionOne {
+    public  int[] twoSum(int[] numbers,int target){
+        int firstPointer=0;//第一个指针
+        int secondPointer=numbers.length-1;// 第二个指针
+        while(firstPointer<secondPointer && (numbers[firstPointer]+numbers[secondPointer])!=target){
+            if(numbers[firstPointer]+numbers[secondPointer]>target){
+                secondPointer--;
+            }else{
+                firstPointer++;
+            }
+        }
+        return  new int[]{firstPointer,secondPointer};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,10,22,3,15,9};
+        QuestionOne one = new QuestionOne();
+        int[] results = one.twoSum(nums, 4);
+        System.out.println(results);
+    }
 }
---
+++ src/main/java/com/tfl/chapter2/QuestionTwo.java
@@ -0,0 +1,57 @@
+package com.tfl.chapter2;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 找到数组中和为0的3个数，不允许出现重复的组合
+ * 题目重点：1.数组不是排好序的 2.不能重复所以扫描过的数字需要跳过
+ *  可以把三数之后转成两数之和
+ */
+public class QuestionTwo {
+    public static void main(String[] args) {
+        int[] nums ={12,3,37,-3,0,-4,-37};
+        QuestionTwo two = new QuestionTwo();
+        List<List<Integer>> lists = two.threeSum(nums);
+        System.out.println(lists);
+    }
+
+    public List<List<Integer>>  threeSum(int nums[]){
+        List<List<Integer>> result = new LinkedList<List<Integer>>();
+
+        if(nums.length>3){
+            Arrays.sort(nums);
+        }
+        int i=0;
+        while (i<nums.length-2){
+            int currNum = nums[i];
+            twoSum(i,result,nums);
+            // 为了跳过重复数字的
+            while(nums[i]==currNum && i<nums.length-2){
+                i++;
+            }
+        }
+        return  result;
+    }
+
+    // 得到两个数的和并存结果
+    private void twoSum(int i, List<List<Integer>> result, int[] nums) {
+        int m = i+1;
+        int n = nums.length-1;
+        // 计算两数之和是否为某个数
+        while(m<n){
+            if (nums[i]+nums[m]+nums[n] == 0){
+                result.add(Arrays.asList(nums[i],nums[m],nums[n]));
+                int currNum = nums[m];
+                while(nums[m]==currNum && m<n){
+                    m++;
+                }
+            }else if(nums[i]+nums[m]+nums[n] > 0){
+                n--;
+            }else{
+                m++;
+            }
+        }
+    }
+}
