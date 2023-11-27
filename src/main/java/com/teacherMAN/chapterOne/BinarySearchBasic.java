5536b28b2613721acac85cd26422bc8d5393bb76
xiaoliang <651147191@qq.com>
2023-11-27 17:56:41 +0800

    二分查找法改进

--- src/main/java/com/teacherMAN/chapterOne/BinarySearchBasic.java
+++ src/main/java/com/teacherMAN/chapterOne/BinarySearchBasic.java
@@ -4,6 +4,7 @@ public class BinarySearchBasic {
     /**
      * 需要注意的问题
      * 1. 整数越界问题 采用无符号右移解决·
+     * 要是没有找到目标元素，左索引为插入点。
      * @param arr
      * @param target
      * @return
@@ -36,5 +37,79 @@ public class BinarySearchBasic {
             }
         }
         return -1;
+    }
+
+    /**
+     * 找到最左侧代码
+     * @param arr
+     * @param target
+     * @return
+     */
+    public static  int binarySearchMostLeftBasic(int arr[],int target){
+        int left = 0;
+        int right = arr.length-1;
+        int index = -1;
+        while(left<=right){
+            int mid = (right+left)>>>1;
+            if(arr[target]<arr[mid]){
+                right  = mid-1;
+            }else if(arr[mid] < target){
+                left = mid+1;
+            }else {
+                index = mid;
+                right = mid-1; // 求最左边的数 所以需要继续向左边的元素开始排查
+            }
+        }
+        if(index >0){
+            return  index;
+        }
+        return -(left+1);
+    }
+
+    /**
+     * 可以直接返回左指针 ；代表了 ：如果找到就是索引，没有找到就返回带查找元素的位置的索引+1的地方；
+     * @param arr
+     * @param target
+     * @return  返回的是 >= target的最靠左的索引
+     */
+    public static  int binarySearchMostLeft(int arr[],int target){
+        int left = 0;
+        int right = arr.length-1;
+        while(left<=right){
+            int mid = (right+left)>>>1;
+            if(arr[target]<=arr[mid]){
+                right  = mid-1;
+            }else if(arr[mid] < target){
+                left = mid+1;
+            }
+        }
+        return  left;
+    }
+
+    /**
+     *
+     * @param arr
+     * @param target
+     * @return 返回的是<= target的最靠右边的索引
+     */
+    public static  int binarySearchMostRight(int arr[],int target){
+        int left = 0;
+        int right = arr.length-1;
+        int index = -1;
+        while(left<=right){
+            int mid = (right+left)>>>1;
+            if(arr[target]<arr[mid]){
+                right  = mid-1;
+            }else if(arr[mid] < target){
+                left = mid+1;
+            }else {
+                index = mid;
+                left = mid+1; // 求最左边的数 所以需要继续向左边的元素开始排查
+            }
+        }
+        if(index >0){
+            return  index;
+        }
+        return -(left+1);
     }
 }
